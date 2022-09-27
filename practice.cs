1c669702d19385fc0e59a55b564916b7089d9e8d
Abhishek Singh <57562968+iashek@users.noreply.github.com>
2022-09-27 20:17:35 +0530

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -1,1 +1,20 @@
 
+using System
+
+public class Practice
+{
+    public static void Main(string[] args)
+    {
+        LinkedList<int> list=new LinkedList<int>();
+        for(int i=0;i<5;i++)
+          list.AddLast(i*10);
+        for(int i=5;i<10;i++)
+          list.AddFirst(i*10);
+        
+        foreach(int i in list)
+            Console.Write(i+"   ");
+        Console.WriteLine();
+        
+        
+    }
+}
