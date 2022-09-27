925037aa1c0a61f1bbe8096d7541323ec7c39484
Abhishek Singh <57562968+iashek@users.noreply.github.com>
2022-09-27 20:31:19 +0530

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -35,13 +35,56 @@ public class Practice
         // Printing the Count of firstlist
         Console.WriteLine("Count Is: " + firstlist.Count);
         
+        // Output:
+        // Capacity Is: 4
+        // Count Is: 4
+        // Capacity Is: 8
+        // Count Is: 6
+        
+        // Creating an List<T> of Integers
+        List<int> secondlist = new List<int>();
+  
+        // Adding elements to List
+        secondlist.Add(2);
+        secondlist.Add(4);
+        secondlist.Add(7);
+        secondlist.Add(2);
+        secondlist.Add(6);
+        secondlist.Add(2);
+        secondlist.Add(2);
+  
+        Console.WriteLine("Elements Present in List:\n");
+  
+        // Displaying the elements of List
+        foreach(int k in secondlist)
+        {
+            Console.WriteLine(k);
+        }
+  
+        Console.WriteLine(" ");
+  
+        Console.Write("Result: ");
+  
+        // Will give the first occurrence of the
+        // element of firstlist that match the
+        // conditions defined by predicate
+        Console.WriteLine(secondlist.Find(isEven));
+        
+//         Output:
+//         Elements Present in List:
+
+//         2
+//         4
+//         7
+//         2
+//         6
+//         2
+//         2
+
+//         Result: 2
+        
     }
 }
 
-// Output:
 
-// Capacity Is: 4
-// Count Is: 4
-// Capacity Is: 8
-// Count Is: 6
 
